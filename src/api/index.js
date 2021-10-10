7ad17a821644c67579c436db8ce4d097dd0d6fba
sehermahmud <seher.mahmud@gmail.com>
2021-10-10 19:59:01 +0600

    Added

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4002' });
+const API = axios.create({ baseURL: 'https://sis-vs-sis.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
